d7099a39fcaaa72ef2453a1d46f3cb8c1258c713
Andre Hofmann <gumengangler@gmail.com>
2017-12-19 23:04:28 +0100

    initial project's content

---
+++ src/main/java/net/andreho/agent/ClassFileTransformerService.java
@@ -0,0 +1,13 @@
+package net.andreho.agent;
+
+import java.lang.instrument.ClassFileTransformer;
+
+/**
+ * <br/>Created by a.hofmann on 16.06.2017 at 20:01.
+ */
+public interface ClassFileTransformerService extends ClassFileTransformer, Comparable<ClassFileTransformerService> {
+  @Override
+  default int compareTo(ClassFileTransformerService o) {
+    return 0;
+  }
+}
